4143b055533d7adaf11205e303f09f9627e3b251
Dulanjan Sithmina <dulanjansithmina@gmail.com>
2025-11-16 18:50:50 +0530

    Modifing BubbleSortAnalyzer.java

--- src/BubbleSortAnalyzer.java
+++ src/BubbleSortAnalyzer.java
@@ -1,52 +1,49 @@
 import java.util.Random;
 
 public class BubbleSortAnalyzer {
-   public BubbleSortAnalyzer() {
-   }
 
-   public static void main(String[] var0) {
-      int[] var1 = new int[]{100, 500, 1000};
-      System.out.println("Algorithm: Bubble Sort");
-      System.out.println("Input Size | Time (ms)");
-      System.out.println("----------------------");
-      int[] var2 = var1;
-      int var3 = var1.length;
+    // Main Method – runs bubble sort for different array sizes
+    public static void main(String[] args) {
+        int[] sizes = {100, 500, 1000};
 
-      for(int var4 = 0; var4 < var3; ++var4) {
-         int var5 = var2[var4];
-         int[] var6 = generateRandomArray(var5);
-         long var7 = System.nanoTime();
-         bubbleSort(var6);
-         long var9 = System.nanoTime();
-         double var11 = (double)(var9 - var7) / 1000000.0;
-         System.out.printf("%-10d | %.4f%n", var5, var11);
-      }
+        System.out.println("Algorithm: Bubble Sort");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
 
-   }
+        for (int size : sizes) {
+            // Random Value Generator
+            int[] arr = generateRandomArray(size);   
 
-   static int[] generateRandomArray(int var0) {
-      Random var1 = new Random();
-      int[] var2 = new int[var0];
+            long start = System.nanoTime();
+            // Bubble Sort execution
+            bubbleSort(arr);                         
+            long end = System.nanoTime();
 
-      for(int var3 = 0; var3 < var0; ++var3) {
-         var2[var3] = var1.nextInt(1000);
-      }
+            double timeMs = (end - start) / 1_000_000.0;
+            System.out.printf("%-10d | %.4f%n", size, timeMs);
+        }
+    }
 
-      return var2;
-   }
+    // Random Value Generator – creates an array with random integers
+    static int[] generateRandomArray(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = rand.nextInt(1000);
+        return arr;
+    }
 
-   static void bubbleSort(int[] var0) {
-      int var1 = var0.length;
-
-      for(int var2 = 0; var2 < var1 - 1; ++var2) {
-         for(int var3 = 0; var3 < var1 - var2 - 1; ++var3) {
-            if (var0[var3] > var0[var3 + 1]) {
-               int var4 = var0[var3];
-               var0[var3] = var0[var3 + 1];
-               var0[var3 + 1] = var4;
+    // Bubble Sort Method – sorts the array using bubble sort logic
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
-         }
-      }
-
-   }
+        }
+    }
 }
